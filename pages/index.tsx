ab71da852d925a46828d03019688820098515490
Kim Jioo <jwkim101201@gmail.com>
2022-03-29 16:56:32 +0900

    refactor: change doublequote to singlequote

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className='text-3xl font-bold underline'>
         어쩔티비
       </h1>
     </div>
