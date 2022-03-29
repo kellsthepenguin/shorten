14d37e1886aa6fba1a4505fd36efd8f6055066fc
Kim Jioo <jwkim101201@gmail.com>
2022-03-30 06:37:27 +0900

    fix: vh -> vw

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>
-        어쩔티비
-      </h1>
+      <p className='relative left-8 top-6 font-semibold'>
+        Shorten!
+      </p>
+      <div className='flex justify-center items-center min-h-[60vh] flex-col'>
+        <p className='font-semibold text-[64pt] sm:text-[20pt] text-[15pt]'>
+          Shorten your link!
+        </p>
+        <input className='bg-[#F5F5F5] text-center w-[50vw] h-[10vh] rounded-[20px]' />
+      </div>
     </div>
   )
 }
