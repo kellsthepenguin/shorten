67cc9c501bf2be387b8081fed04f66aa47fd20ec
Kim Jioo <jwkim101201@gmail.com>
2022-03-31 07:29:08 +0900

    add(shortenbutton): hover

--- components/ShortenButton.tsx
+++ components/ShortenButton.tsx
@@ -1,8 +1,8 @@
 const ShortenButton = () => {
   return (
-    <a className='px-[5vw] py-[1vh] bg-[#1f78ff] font-semibold text-[20pt] rounded-[10px] text-white'>
+    <button className='px-[5vw] py-[1vh] bg-blue-500 hover:bg-blue-600 font-semibold text-[20pt] rounded-[10px] text-white'>
       Shorten!
-    </a>
+    </button>
   )
 }
 
