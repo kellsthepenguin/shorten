78744a13e89529e7636de805062cfa11409aecca
Kim Jioo <jwkim101201@gmail.com>
2022-04-03 12:56:47 +0900

    fix: fix ref type

--- components/LinkInput.tsx
+++ components/LinkInput.tsx
@@ -1,6 +1,6 @@
-import { MutableRefObject } from 'react'
+import { RefObject } from 'react'
 
-const LinkInput = ({ ref }: { ref?: MutableRefObject<HTMLInputElement> }) => {
+const LinkInput = ({ ref }: { ref?: RefObject<HTMLInputElement> }) => {
   return (
     <input
       className='outline-none bg-[#F5F5F5] border border-gray-300 px-[30px] md:w-1/2 w-3/5 h-28 rounded-[20px] text-[20pt] font-bold'
