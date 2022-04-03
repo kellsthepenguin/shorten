8701075f4fac5465da7f4521e514ca6bc0f7776f
Kim Jioo <jwkim101201@gmail.com>
2022-04-03 14:41:36 +0900

    feat(frontend): shorten

--- components/ShortenButton.tsx
+++ components/ShortenButton.tsx
@@ -1,6 +1,6 @@
-const ShortenButton = () => {
+const ShortenButton = ({ onClick }: { onClick: any }) => {
   return (
-    <button className='px-[5vw] py-[1vh] bg-blue-500 hover:bg-blue-600 font-semibold text-[20pt] rounded-[10px] text-white'>
+    <button className='px-[5vw] py-[1vh] bg-blue-500 hover:bg-blue-600 font-semibold text-[20pt] rounded-[10px] text-white' onClick={onClick}>
       Shorten!
     </button>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,41 @@
 import type { NextPage } from 'next'
+import { useRef } from 'react'
 import LinkInput from '../components/LinkInput'
 import ShortenButton from '../components/ShortenButton'
 
 const Home: NextPage = () => {
+  const linkInput = useRef<HTMLInputElement>(null)
+  const shortenedLinkText = useRef<HTMLParagraphElement>(null)
+
+  const handleShorten = () => {
+    const linkToShorten = linkInput.current?.value
+
+    fetch('/api/shorten', {
+      method: 'POST',
+      body: JSON.stringify({ url: linkToShorten }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (shortenedLinkText.current !== null) {
+          console.log(result)
+          shortenedLinkText.current.innerHTML = new URL(location.href).origin + '/' + result.id
+        }
+      })
+  }
+
   return (
     <div className='overflow-hidden'>
-      <p className='relative left-8 top-6 font-semibold'>
-        Shorten!
-      </p>
+      <p className='relative left-8 top-6 font-semibold'>Shorten!</p>
       <div className='flex justify-center items-center min-h-[60vh] flex-col'>
         <p className='font-semibold md:text-[48pt] text-[32pt] text-[15pt]'>
           Shorten your link!
         </p>
-        <LinkInput /> <br />
-        <ShortenButton />
+        <LinkInput innerRef={linkInput} /> <br />
+        <ShortenButton onClick={handleShorten} />
+        <p ref={shortenedLinkText}></p>
       </div>
     </div>
   )
